b4b1f4c882af6694035cb8ba332cd6c02018cb54
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-11-18 15:20:51 -0800

    Add logic to validate type of object inside of list is a str

--- test_remove_dups.py
+++ test_remove_dups.py
@@ -6,7 +6,11 @@ from main import remove_dups
     ([], False),
     ("red", False),
     (9, False),
-    ({"red": 1}, False)
+    ({"red": 1}, False),
+    (["red", "red", "blue", 9], False),
+    (["red", "red", "blue", bool], False),
+    (["red", "red", "blue", ["red","blue"]], False),
+    (["red", "red", "blue", {"red": 1}], False)
 ])
 def test_remove_dups(list_test, output):
     assert remove_dups(list_test) == output
