8a20d9905ccbba6c83f9ba024d20651af7a0edfe
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-11-18 12:01:46 -0800

    WIP: refactoring code to including more comprehensions

--- main.py
+++ main.py
@@ -3,33 +3,31 @@ import json
 
 fake = Faker()
 
-color_list = []
+value_or_more = 4
+color_list = [fake.color_name() for item in range(20)]
 
-for item in range(20): 
-    color = fake.color_name()
-    color_list.append(color)
-
-#print(f"{len(color_list)}: {color_list}")
+print(f"{len(color_list)}: {color_list}")
 
 def remove_dups(list_input:list) -> list:
-    """ Takes in a list, removes duplicate items, and returns list without duplicates
+    """ Validates that argument is list, and if so removes duplicate items, and returns list without duplicates
 
     Args:
         list_input (list): list of strings
 
     Returns:
-        list: Returns list that is absent any duplicates
+        list: Returns list that is absent any duplicates, or if argument was incorrect returns False
     """
     if not list_input:
         return False
     elif isinstance(list_input, list):
-        color_list_no_duplicates = list(set(list_input)) # Remove duplicates by converting to set then back to list
+        color_dict_no_duplicates = {color: None for color in list_input}
+        color_list_no_duplicates = list(color_dict_no_duplicates.keys())
         return color_list_no_duplicates
     else:
         return False
 
 color_list = remove_dups(color_list)
-#print(f"{len(color_list)}: {color_list}")
+print(f"{len(color_list)}: {color_list}")
 
 if color_list == False:
     print("Please input a list with strings inside")
@@ -44,4 +42,4 @@ else:
         color_dict = json.load(json_file)
         print(color_dict)
 
-        greater_four_values = [print(f"{key}: length of color name is {value}") for key, value in color_dict.items() if value >= 4]
+        greater_four_values = [print(f"{key}: length of color name is {value}") for key, value in color_dict.items() if value >= value_or_more]
--- test_remove_dups.py
+++ test_remove_dups.py
@@ -1,10 +1,12 @@
 import pytest
 from main import remove_dups
 
-@pytest.mark.parametrize("list_input, output", [
-    (["red", "blue", "red"], ["blue", "red"]),
+@pytest.mark.parametrize("list_test, output", [
+    (["red", "red", "blue"], ["red","blue"]),
     ([], False),
     ("red", False),
+    (9, False),
+    ({"red": 1}, False)
 ])
-def test_remove_dups(list_input, output):
-    assert remove_dups(list_input) == output
+def test_remove_dups(list_test, output):
+    assert remove_dups(list_test) == output
